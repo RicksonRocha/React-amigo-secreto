e94ec77a0f34f1192faa6cd2c3b50ecf09dcae8a
Rickson <ricksonrocha0@gmail.com>
2023-01-10 17:41:14 -0300

    :sparkles::recycle: Adição de alerta para quantidade de participantes

--- src/componentes/Lista/ListaParticipantes.test.tsx
+++ src/componentes/Lista/ListaParticipantes.test.tsx
@@ -11,7 +11,6 @@ jest.mock('../../state/hook/useListaDeParticipantes', () => {
 })
 
 describe('Uma lista vazia de participantes', () => {
-
     beforeEach(() => {
         (useListaDeParticipantes as jest.Mock).mockReturnValue([])
     })
@@ -24,15 +23,18 @@ describe('Uma lista vazia de participantes', () => {
     
         const itens = screen.queryAllByRole('listitem')
         expect(itens).toHaveLength(0)
+
+        const mensagemParticipantes = screen.getByRole('alert')
+        expect(mensagemParticipantes).toHaveTextContent('Mínimo de 3 participantes para iniciar')
     })
 })
 
 describe('Uma lista preenchida de participantes', () => {
-    const participantes = ['Rickson', 'Juliane']
+    const participantes = ['Daenerys', 'Rhaenyra', 'Cersei']
     beforeEach(() => {
         (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes)
     })
-    test('deve ser renderizada com 2 elementos', () => {
+    test('deve ser renderizada com 3 elementos', () => {
         render(
             <RecoilRoot>
                 <ListaParticipantes />
@@ -41,5 +43,33 @@ describe('Uma lista preenchida de participantes', () => {
     
         const itens = screen.queryAllByRole('listitem')
         expect(itens).toHaveLength(participantes.length)
+        const mensagemParticipantes = screen.getByRole('alert')
+        expect(mensagemParticipantes).toHaveTextContent('Pronto para iniciar!')
+    })
+})
+
+describe('Uma lista com 1 participante', () => {
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(['Daenerys'])
+    })
+    test('deve requerir mais 2 participantes', () => {
+        render(
+            <RecoilRoot>
+                <ListaParticipantes />
+            </RecoilRoot>
+        )
+        const mensagemParticipantes = screen.getByRole('alert')
+        expect(mensagemParticipantes).toHaveTextContent('Faltam 2 participantes para iniciar')
+    })
+})
+
+describe('Uma lista com 2 participantes', () => {
+    beforeEach(() => {
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(['Daenerys', 'Rhaenyra'])
+    })
+    test('deve requerir mais 1 participante', () => {
+        render(<RecoilRoot><ListaParticipantes /></RecoilRoot>)
+        const mensagemParticipantes = screen.getByRole('alert')
+        expect(mensagemParticipantes).toHaveTextContent('Falta 1 participante para iniciar')
     })
 })
--- src/componentes/Lista/ListaParticipantes.tsx
+++ src/componentes/Lista/ListaParticipantes.tsx
@@ -3,8 +3,22 @@ import { useListaDeParticipantes } from '../../state/hook/useListaDeParticipante
 
 export const ListaParticipantes = () => {
     const participantes: string[] = useListaDeParticipantes()
+    const quantidade = participantes.length
+    const mensagem = {
+        minimo: 'Mínimo de 3 participantes para iniciar',
+        faltaUm: `Falta 1 participante para iniciar`,
+        faltaDois: `Faltam 2 participantes para iniciar`,
+        prontos: 'Pronto para iniciar!'
+    }
+    
     return (
         <ul>
+            {quantidade < 3 
+                ? (quantidade === 0 
+                    ? <p role="alert">{mensagem['minimo']}</p> 
+                    : <p role="alert">{quantidade === 2 ? mensagem['faltaUm'] : mensagem['faltaDois']}</p>) 
+                : <p role="alert">{mensagem['prontos']}</p>
+            }
             {participantes.map((participante, idx) => <li key={idx}>{participante}</li>)}
         </ul>
     )
